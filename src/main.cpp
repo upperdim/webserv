a19cb8fb2e488cec85087577001eb0b3fd44b3d2
nmihaile <nmihaile@student.42heilbronn.de>
2025-02-20 17:45:42 +0100

    updates main.cpp and uses the Server class for simple HTTP-flavored echo server

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/18 11:44:18 by nmihaile          #+#    #+#             */
+/*   Updated: 2025/02/20 17:44:58 by nmihaile         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "../inc/Server.hpp"
 
 int	main(void)
 {
+
+	Server server;
+
+	std::cout << "\033[92mservering the web…\033[0m" << std::endl;
+	while (42)
+	{
+		server.srv_listen();
+	}
 
 	return (0);
 }
