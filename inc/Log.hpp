b4e0dfa9a2cb4b6da2338d0392ffc07af3a35553
nmihaile <nmihaile@student.42heilbronn.de>
2025-02-22 14:29:09 +0100

    add class Log

---
+++ inc/Log.hpp
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Log.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/22 13:14:44 by nmihaile          #+#    #+#             */
+/*   Updated: 2025/02/22 13:22:53 by nmihaile         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LOG_HPP
+#define LOG_HPP
+
+#include <string>
+#include <iostream>
+#include "webserv.h"
+
+class Log
+{
+public:
+	~Log();
+
+	static void	debug(const std::string msg);
+	static void	info(const std::string msg);
+	static void	warning(const std::string msg);
+	static void	error(const std::string msg);
+	static void	success(const std::string msg);
+
+	typedef struct s_type
+	{
+		std::string	name;
+		std::string	col1;
+		std::string	col2;
+	} t_type;
+
+private:
+	Log();
+	Log(const Log& orher);
+	Log&	operator=(const Log& rhs);
+
+	static const size_t DEBUG	= 0;
+	static const size_t INFO	= 1;
+	static const size_t WARNING	= 2;
+	static const size_t ERROR	= 3;
+	static const size_t SUCCESS	= 4;
+
+	static const t_type LOG_TYPES[5];
+
+	static void	print(const t_type type, const std::string msg);
+};
+
+#endif
---
+++ src/Log.cpp
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Log.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/22 13:16:39 by nmihaile          #+#    #+#             */
+/*   Updated: 2025/02/22 13:22:51 by nmihaile         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Log.hpp"
+
+Log::~Log()
+{
+}
+
+const Log::t_type Log::LOG_TYPES[5] = {
+	{std::string("DEBUG"), RESET, RESET},
+	{std::string("INFO"), LIGHTBLUE, BLUE},
+	{std::string("WARNING"), LIGHTYELLOW, YELLOW},
+	{std::string("ERROR"), LIGHTRED, RED},
+	{std::string("SUCCESS"), LIGHTGREEN, GREEN}
+};
+
+
+/* ************************************************************************** */
+/* ************************************************************************** */
+
+
+void	Log::debug(const std::string msg)
+{
+	print(LOG_TYPES[DEBUG], msg);
+}
+
+void	Log::info(const std::string msg)
+{
+	print(LOG_TYPES[INFO], msg);
+}
+
+void	Log::warning(const std::string msg)
+{
+	print(LOG_TYPES[WARNING], msg);
+}
+
+void	Log::error(const std::string msg)
+{
+	print(LOG_TYPES[ERROR], msg);
+}
+
+void	Log::success(const std::string msg)
+{
+	print(LOG_TYPES[SUCCESS], msg);
+}
+
+void	Log::print(const t_type type, const std::string msg)
+{
+	std::cerr	<< type.col1 << BOLD << "[" << type.name << "] "
+				<< type.col2 << REGULAR << msg
+				<< RESET << std::endl;
+}
