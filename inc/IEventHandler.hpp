7b8daab859e58f502a01c5fb9823e32e8db65c3d
nmihaile <nmihaile@student.42heilbronn.de>
2025-02-23 15:07:51 +0100

    adds handleDisConnectEvent() to IEventHandler Interface

--- inc/IEventHandler.hpp
+++ inc/IEventHandler.hpp
@@ -6,12 +6,16 @@
 /*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 19:42:07 by nmihaile          #+#    #+#             */
-/*   Updated: 2025/02/22 19:56:16 by nmihaile         ###   ########.fr       */
+/*   Updated: 2025/02/23 12:08:34 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef IEVENTHANDLER_HPP
 #define IEVENTHANDLER_HPP
+
+#include "EventManager.hpp"
+
+class EventManager;
 
 class IEventHandler
 {
@@ -20,9 +24,10 @@ public:
 	
 	virtual ~IEventHandler() {};
 
-	virtual void handleReadEvent()	=0;
-	virtual void handleWriteEvent()	=0;
-	virtual void handleErrorEvent()	=0;
+	virtual void handleReadEvent(EventManager& event_manager)		=0;
+	virtual void handleWriteEvent(EventManager& event_manager)		=0;
+	virtual void handleErrorEvent(EventManager& event_manager)		=0;
+	virtual void handleDisConnectEvent(EventManager& event_manager)	=0;
 };
 
 #endif
