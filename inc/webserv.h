20011c5bc39e2ce25a8f0d93cc448223a36d8731
nmihaile <nmihaile@student.42heilbronn.de>
2025-02-25 10:59:15 +0100

    adds CYAN color to webserv.h

--- inc/webserv.h
+++ inc/webserv.h
@@ -6,7 +6,7 @@
 /*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/21 13:26:18 by nmihaile          #+#    #+#             */
-/*   Updated: 2025/02/23 11:50:23 by nmihaile         ###   ########.fr       */
+/*   Updated: 2025/02/25 10:58:18 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,13 @@
 # define YELLOW			"\033[33m"
 # define BLUE			"\033[34m"
 # define MAGENTA		"\033[35m"
+# define CYAN			"\033[36m"
 # define LIGHTRED		"\033[91m"
 # define LIGHTGREEN		"\033[92m"
 # define LIGHTYELLOW	"\033[93m"
 # define LIGHTBLUE		"\033[94m"
 # define LIGHTMAGENTA	"\033[95m"
+# define LIGHTCYAN		"\033[96m"
 # define RESET			"\033[0m"
 # define DEFAULT		"\033[0m"
 
