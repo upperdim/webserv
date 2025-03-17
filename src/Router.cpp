b6f265a4ba81ef30f62670dfeefb61772065ef72
nmihaile <nmihaile@student.42heilbronn.de>
2025-03-17 19:08:07 +0100

    removes Errorhandler from static const m_handlers in Router.cpp

--- src/Router.cpp
+++ src/Router.cpp
@@ -6,7 +6,7 @@
 /*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 14:26:26 by nmihaile          #+#    #+#             */
-/*   Updated: 2025/03/14 16:36:28 by nmihaile         ###   ########.fr       */
+/*   Updated: 2025/03/17 18:49:01 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,17 +27,11 @@ Router::~Router()
 
 
 const std::map<std::string, std::function<AHandler*(const Server&)>>	Router::m_handlers = {
-	{"GET", createGetHandler},
-	{"ERROR", createErrorHandler}
+	{"GET", createGetHandler}
 };
 
 AHandler*	Router::route(const Request& request)
 {
-	// static const std::map<std::string, std::function<AHandler*(void)>>	m_handlers = {
-	// 	{"GET", createGetHandler},
-	// 	{"ERROR", createErrorHandler}
-	// };
-
 	if (request.error())
 		return ( createErrorHandler(m_server) );
 
