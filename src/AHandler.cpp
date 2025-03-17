162522024966983a5c0f861011c2fb12fdf68a79
nmihaile <nmihaile@student.42heilbronn.de>
2025-03-17 19:27:40 +0100

    updates the AHandler createErrorResponse to use the new response.setBodyString() func

--- src/AHandler.cpp
+++ src/AHandler.cpp
@@ -6,7 +6,7 @@
 /*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/09 17:47:24 by nmihaile          #+#    #+#             */
-/*   Updated: 2025/03/14 16:22:25 by nmihaile         ###   ########.fr       */
+/*   Updated: 2025/03/15 18:59:07 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ void	AHandler::createErrorResponse(Response& response, int _status_code)
 	response.setProtokoll("HTTP/1.1");
 	response.setStatus(_status_code, HTTP::getStatusMessage(_status_code));
 	response.addHeader("Content-Type", HTTP::getMimeType(".html"));
-	response.setBody(fetchErrorPage(_status_code));
+	response.setBodyString(fetchErrorPage(_status_code));
 }
 
 std::string	AHandler::fetchErrorPage(int _status_code) const
