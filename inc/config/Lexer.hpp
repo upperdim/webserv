bfd3d0de8aed43a4746d783114e16a672a879e54
nmihaile <nmihaile@student.42heilbronn.de>
2025-06-04 15:30:34 +0200

    adds inital Lexer class

---
+++ inc/config/Lexer.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Lexer.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/04 11:38:27 by nmihaile          #+#    #+#             */
+/*   Updated: 2025/06/04 15:28:56 by nmihaile         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LEXER_HPP
+#define LEXER_HPP
+
+#include <string>
+#include "Log.hpp"
+
+class Lexer
+{
+public:
+	Lexer(const std::string& _input);
+	Lexer(const Lexer& other);
+	~Lexer();
+
+private:
+	Lexer& operator=(const Lexer& rhs);
+
+	std::string	m_input;
+	size_t		m_pos;
+};
+
+#endif
