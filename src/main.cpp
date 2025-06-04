6be6aa9c5735f01ef0250dd1ac376dd3a7853933
nmihaile <nmihaile@student.42heilbronn.de>
2025-06-04 20:12:30 +0200

    uses Lexer and Parser in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,23 +6,47 @@
 /*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 11:44:18 by nmihaile          #+#    #+#             */
-/*   Updated: 2025/03/13 18:33:42 by nmihaile         ###   ########.fr       */
+/*   Updated: 2025/06/04 19:25:23 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "webserv.hpp"
+#include "Parser.hpp"
 #include "EventManager.hpp"
 #include "Server.hpp"
+#include "Log.hpp"
 
 bool	g_running = true;
 
-int	main(void)
+void	terminate(std::string msg, int exit_code)
 {
+	Log::info(msg);
+	exit(exit_code);
+}
+
+int	main(int ac, char **av)
+{
+	if (ac != 2)
+		terminate("usage: ./webserv config.conf", 0);
+
 	std::signal(SIGINT, handleAbort);
 	std::signal(SIGQUIT, handleAbort);
 
 	try
 	{
+		Lexer	lexer(readFile(av[1]));
+		Parser	parser(lexer);
+		std::string		config;	// TODO: change to config struct
+
+		try
+		{
+			config = parser.parse();
+		}
+		catch (const std::exception& e)
+		{
+			terminate(e.what(), 1);
+		}
+
 		EventManager event_manager(g_running);
 		Server* server = new Server();
 
