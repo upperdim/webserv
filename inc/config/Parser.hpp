f09b99f531e01e1969f1aa15e9e3d7cb62648699
nmihaile <nmihaile@student.42heilbronn.de>
2025-06-06 11:08:51 +0200

    parses the config file, skips the events directive, handles http directive, but none internat directives like server or location

--- inc/config/Parser.hpp
+++ inc/config/Parser.hpp
@@ -6,7 +6,7 @@
 /*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 11:05:43 by nmihaile          #+#    #+#             */
-/*   Updated: 2025/06/05 18:01:19 by nmihaile         ###   ########.fr       */
+/*   Updated: 2025/06/06 10:25:41 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,13 +26,16 @@ public:
 	Parser(const Lexer& _lexer);
 	~Parser();
 
-	std::string	parse();	// TODO: return config struct
+	std::string	parse(void);	// TODO: return config struct
 
 private:
 	Parser(const Parser& other);
 	Parser&	operator=(const Parser& rhs);
 
-	Lexer			m_lexer;
+	void	consume(TokenType _type, std::string msg = "");
+
+	Lexer		m_lexer;
+	Token		m_currentToken;
 	// Config			m_config;
 };
 
--- src/config/Parser.cpp
+++ src/config/Parser.cpp
@@ -6,14 +6,15 @@
 /*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 11:05:42 by nmihaile          #+#    #+#             */
-/*   Updated: 2025/06/04 19:30:26 by nmihaile         ###   ########.fr       */
+/*   Updated: 2025/06/06 11:07:06 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Parser.hpp"
 
 Parser::Parser(const Lexer& _lexer)
-	:	m_lexer(_lexer)
+	:	m_lexer(_lexer),
+		m_currentToken(TokenType::END_OF_INPUT, "")
 {
 }
 
@@ -28,25 +29,63 @@ Parser::~Parser()
 
 std::string	Parser::parse()	// TODO: return config struct
 {
-	// m_file_path = _file_path;
-	// m_ifs.open(m_file_path);
 
-	// if (!m_ifs.is_open())
-	// 	throw ( std::invalid_argument(std::string("failed to open file: ") + m_file_path) );
+	// DEBUG: => READ ALL TOKENS AND LOG THEM OUT
+	// m_currentToken = m_lexer.nextToken();
+	// while (m_currentToken.type != TokenType::END_OF_INPUT && m_currentToken.type != TokenType::ERROR)
+	// {
+	// 	LOG_INFO( m_currentToken.toString() );
+	// 	m_currentToken = m_lexer.nextToken();
+	// }
+	// LOG_INFO( m_currentToken.toString() );
 
-	// m_ifs.close();
 
-
-	// read all tokens and log them out
-	Token currentToken = m_lexer.nextToken();
-	while (currentToken.type != TokenType::END_OF_INPUT && currentToken.type != TokenType::ERROR)
+	m_currentToken = m_lexer.nextToken();
+	while (m_currentToken.type != TokenType::END_OF_INPUT)
 	{
-		LOG_INFO( currentToken.toString() );
-		currentToken = m_lexer.nextToken();
-	}
-	LOG_INFO( currentToken.toString() );
+		switch (m_currentToken.type)
+		{
+			case (TokenType::EVENTS):
+			{
+				LOG_INFO("\033[92mwebserv\033[94m doesn't support the 'EVENTS' directive.");
+				while (m_currentToken.type != TokenType::CLOSE_BRACE && m_currentToken.type != TokenType::END_OF_INPUT && m_currentToken.type != TokenType::ERROR)
+					m_currentToken = m_lexer.nextToken();
+				if (m_currentToken.type == TokenType::END_OF_INPUT)
+					throw ( std::runtime_error("webserv: unexpected end of file, expected `}' after 'EVENTS' directive.") );
+				else if (m_currentToken.type == TokenType::ERROR)
+					throw ( std::runtime_error("webserv: an error occured while skipping unsupported 'EVENTS' directive.") );
+				break ;
+			}
+			case (TokenType::HTTP):
+			{
+				m_currentToken = m_lexer.nextToken();
+				consume(TokenType::OPEN_BRACE, "webserv: invalid number of arguments in \"http\" directive, OPEN_BRACE `{' expected");
 
+				///////////////////////////////////////////////
+				//////////////continue here////////////////////
+				///////////////////////////////////////////////
+		
+				break ;
+			}
+			default:
+				throw ( std::runtime_error(std::string("webserv: unknown directive ") + m_currentToken.value ) );
+		}
+		
+		m_currentToken = m_lexer.nextToken();
+	}
 
 	// return (m_config);
 	return (std::string(""));
+}
+
+
+/* ************************************************************************** */
+/* ************************************************************************** */
+
+
+void	Parser::consume(TokenType _type, std::string msg)
+{
+	if (m_currentToken.type != _type)
+		throw ( std::runtime_error(msg) );
+	m_currentToken = m_lexer.nextToken();
 }
