19836ee10a4c9975a544dc40dae7341a7ee99072
nmihaile <nmihaile@student.42heilbronn.de>
2025-06-09 16:53:00 +0200

    updates main syntax

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 11:44:18 by nmihaile          #+#    #+#             */
-/*   Updated: 2025/06/07 14:17:59 by nmihaile         ###   ########.fr       */
+/*   Updated: 2025/06/09 16:52:00 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,21 +45,20 @@ int	main(int argc, char **argv)
 	std::signal(SIGINT, handleAbort);
 	std::signal(SIGQUIT, handleAbort);
 
-	try
-	{
+	Config config;
+
+	try	{
 		Lexer	lexer(readFile(argv[1]));
 		Parser	parser(lexer);
-		Config config = parser.mockParseConfig(configFilePath);
 
-		try
-		{
-			config = parser.parse();
-		}
-		catch (const std::exception& e)
-		{
-			terminate(e.what(), 1);
-		}
+		// config = parser.parse();
+		config = parser.mockParseConfig(configFilePath);
+		
+	} catch (const std::exception& e)	{
+		terminate(e.what(), 1);
+	}
 
+	try {
 		EventManager event_manager(g_running);
 		Server* server = new Server();
 
@@ -67,9 +66,7 @@ int	main(int argc, char **argv)
 		event_manager.processPendingRegistrations();
 
 		event_manager.run();
-	}
-	catch(const std::exception& e)
-	{
+	} catch(const std::exception& e) {
 		LOG_ERROR_LM("Critical Exception caught","::");
 		LOG_ERROR(e.what());
 		g_running = false;
