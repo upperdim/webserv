324d9b1c6528f4ca6c70064ce8f4aebb391faa32
nmihaile <nmihaile@student.42heilbronn.de>
2025-06-14 14:25:12 +0200

    fixes missing space before filename error msg in src/utils/readFile.cpp

--- src/utils/readFile.cpp
+++ src/utils/readFile.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   readFile.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tunsal <tunsal@student.42.fr>              +#+  +:+       +#+        */
+/*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 15:05:58 by nmihaile          #+#    #+#             */
-/*   Updated: 2025/06/04 15:10:56 by nmihaile         ###   ########.fr       */
+/*   Updated: 2025/06/14 14:23:43 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ std::string readFile(const std::string& filename)
 {
 	std::ifstream file(filename);
 	if (!file)
-		throw ( std::runtime_error("Failed to open" + filename) );
+		throw ( std::runtime_error("Failed to open " + filename) );
 	std::stringstream buffer;
 	buffer << file.rdbuf();
 	return (buffer.str());
