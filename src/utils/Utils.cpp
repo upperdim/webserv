d60cc459ea69adaa77a3650ee2470321dcba1a13
nmihaile <nmihaile@student.42heilbronn.de>
2025-06-19 16:42:10 +0200

    refactors utility functions… moves startsWith() into	Utils class removes src/utils/startsEndsWith.cpp and removes it from src/utils/webserv.hpp introduces Utils::isAllowedMethod() method improves Utils::sanitizePath() method

--- src/utils/Utils.cpp
+++ src/utils/Utils.cpp
@@ -1,23 +1,45 @@
 #include "Utils.hpp"
 
-std::string	Utils::sanitizePath(const Request& request, Response& response, const ServerBlock serverBlock)
+std::string	Utils::sanitizePath(const Request& request, const ServerBlock serverBlock)
 {
-	std::string	root_path	= serverBlock.root; //getRootDir()
+	std::string	root_path	= serverBlock.root;
 	std::string path		= request.getRequestTarget();
 
-	(void)response;	// TODO: delete me
+	if (path == "/") {
+		if (serverBlock.index.empty())
+			path += "index.html";
+		else
+			path += serverBlock.index;
+	}
 
-	if (path == "/")
-		path += "index.html";
+	//	TODO:	do we retirn an Resons error if the User tries to move up the direcoty???
+	// remove the '/' on path and all attempts to move up the director
 	while (startsWith(path, "../"))
 		path.erase(0, 3);
 	while (startsWith(path, "/"))
 		path.erase(0, 1);
-	return (path);
+
+	//	TODO:	Do we allow serverBlock.root to be a file or only a directory ???
+	if (root_path.back() != '/')
+		root_path += '/';
+
+	return root_path + path;
 }
 
 bool	Utils::resourceExist(const std::string& path)
 {
 	std::ifstream	file(path);
 	return ( file.good() );
+}
+
+bool	Utils::startsWith(const std::string& str, const std::string& prefix)
+{
+	if (str.size() < prefix.size())
+		return (false);
+	return ( std::equal(prefix.begin(), prefix.end(), str.begin()) );
+}
+
+bool	Utils::isAllowedMethod(HTTP::Method method, const std::vector<HTTP::Method> allowedMethods)
+{
+	return std::find(allowedMethods.begin(), allowedMethods.end(), method) != allowedMethods.end();
 }
--- src/utils/Utils.hpp
+++ src/utils/Utils.hpp
@@ -10,8 +10,10 @@ class Utils
 {
 public:
 
-	static std::string	sanitizePath(const Request& request, Response& response, const ServerBlock serverBlock);
+	static std::string	sanitizePath(const Request& request, const ServerBlock serverBlock);
 	static bool			resourceExist(const std::string& path);
+	static bool			startsWith(const std::string& str, const std::string& prefix);
+	static bool			isAllowedMethod(HTTP::Method method, const std::vector<HTTP::Method> allowdMethods);
 };
 
 #endif
