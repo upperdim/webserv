e6d4dc5a9e21b64badcd35167cd46776d0ac78e0
nmihaile <nmihaile@student.42heilbronn.de>
2025-06-23 12:24:43 +0200

    refines new Request class adds move assignment operator adds getters: getStatusCode(), getMethod(), getRequestTarget(), getLocation()

--- src/http/Request2.cpp
+++ src/http/Request2.cpp
@@ -6,14 +6,15 @@
 /*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/22 18:06:22 by nmihaile          #+#    #+#             */
-/*   Updated: 2025/06/22 19:55:01 by nmihaile         ###   ########.fr       */
+/*   Updated: 2025/06/23 12:13:52 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Request.hpp"
+#include "Request2.hpp"
 
 Request::Request()
-	:	m_statusCode(200)
+	:	m_statusCode(200),
+		m_method(HTTP::Method::GET)
 {
 }
 
@@ -21,6 +22,46 @@ Request::~Request()
 {
 }
 
+// move assignment operator
+Request&	Request::operator=(Request&& rhs)
+{
+	if (this != &rhs) {
+		m_statusCode	= rhs.m_statusCode;
+		m_requestTarget	= std::move(rhs.m_requestTarget);
+		m_protokoll		= std::move(rhs.m_protokoll);
+		m_method		= rhs.m_method;
+		m_headers		= std::move(rhs.m_headers);
+	}
+	return *this;
+}
+
 
 /* ************************************************************************** */
 /* ************************************************************************** */
+
+int	Request::getStatusCode(void) const
+{
+	return (m_statusCode);
+}
+
+HTTP::Method	Request::getMethod(void) const
+{
+	return (m_method);
+}
+
+std::string	Request::getRequestTarget(void) const
+{
+	return (m_requestTarget);
+}
+
+const LocationBlock&	Request::getLocation(const ServerBlock& serverBlock) const
+{
+	// Matches the requestTaget to a serverBlock.locationBlock
+	// TODO:	more needs o be done
+	for (const auto& locationBlock : serverBlock.locationBlocks) {
+		if (m_requestTarget == locationBlock.route)
+			return locationBlock;
+	}
+	return serverBlock.locationBlocks.front();
+}
+
--- src/http/Request2.hpp
+++ src/http/Request2.hpp
@@ -6,7 +6,7 @@
 /*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/22 18:06:23 by nmihaile          #+#    #+#             */
-/*   Updated: 2025/06/22 19:54:55 by nmihaile         ###   ########.fr       */
+/*   Updated: 2025/06/23 12:13:23 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,12 @@
 #define REQUEST_HPP
 
 #include <string>
+#include <unordered_map>
+#include "HTTP.hpp"
+#include "Config.hpp"
+
+// Forward declaration
+class RequestParser;
 
 class Request
 {
@@ -21,10 +27,21 @@ public:
 	Request();
 	~Request();
 
+	// move assignment operator
+	Request&	operator=(Request&& rhs);
+
+	int						getStatusCode(void) const;
+	HTTP::Method			getMethod(void) const;
+	std::string				getRequestTarget(void) const;
+	const LocationBlock&	getLocation(const ServerBlock& serverBlock) const;
+
 private:
-	int			m_statusCode;
-	std::string	m_requestTarget;
-	std::string	m_protokoll;
+	int												m_statusCode;
+	std::string										m_requestTarget;
+	std::string										m_protokoll;
+
+	HTTP::Method									m_method;
+	std::unordered_map<std::string, std::string>	m_headers;
 
 	friend class RequestParser;
 };
