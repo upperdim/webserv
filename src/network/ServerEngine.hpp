e59befff88a55258db6ce5d923c7a0ff7aee9313
upperdim <45048352+upperdim@users.noreply.github.com>
2025-06-27 02:10:34 +0200

    add header file

---
+++ src/network/ServerEngine.hpp
@@ -0,0 +1,57 @@
+/*
+	ServerEngine
+
+	- Manages servers
+	- Utilizes poll() for async I/O in order to handle multiple connections
+		- Owns and manages pollfd list
+		- Runs the main poll loop
+	- Accepts new connections
+	- Dispatches events to the respective server or client connection
+*/
+
+#ifndef SERVERENGINE_HPP
+#define SERVERENGINE_HPP
+
+#include <unordered_map>
+#include <vector>
+#include "poll.h"
+#include "Server.hpp"
+#include "Config.hpp"
+#include "ClientConnection.hpp"
+
+#define POLL_TIMEOUT_MS		2000
+
+class ServerEngine {
+private:
+	std::vector<Server>							servers;
+	std::unordered_map<int, ClientConnection>	clients;              // Key: fd, value: ClientConnection
+
+	std::vector<struct pollfd>					pollFds;
+	std::vector<int>							pollFdsRegisterQueue; // File descriptors awaiting to be add into pollFds list
+	std::vector<int>							pollFdsRemovalQueue;  // File descriptors awaiting to be remove from pollFds list
+
+	void										addToPollFds(int fd);
+	void										removeFromPollFds(int fd);
+	void										setPollFdEvents(int fd, short events);
+	void										printPollFds();
+
+	void										iteratePollFds(int eventCount);
+	void										updatePollFds();
+	
+	void										acceptNewClientConnection(Server& clientConnectedServer);
+	void										disconnectClient(int clientFd);
+	void										stopServer(int serverFd, int serverIdx);
+	void										readClientIncomingData(int clientFd);
+	void										writeToClient(int clientFd);
+	
+	int											findServerIndexByFd(int fd); // TODO: (Optimization) fd to server map for speed?
+	ClientConnection&							getClientConnectionByFd(int fd);
+	
+public:
+	ServerEngine(Config config);
+	~ServerEngine();
+
+	void										run();
+};
+
+#endif
