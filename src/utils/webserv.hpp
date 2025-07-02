7d40e79d9198eb1137859c36d6bd3f183d264455
upperdim <45048352+upperdim@users.noreply.github.com>
2025-07-02 20:06:03 +0200

    remove unused headers

--- src/utils/webserv.hpp
+++ src/utils/webserv.hpp
@@ -1,21 +1,14 @@
 #ifndef WEBSERV_H
 #define WEBSERV_H
 
+#include <string>
+#include <unordered_map>
+
 // a good default RESPONSE_BUFFER_SIZE seams to be 64kb - 128kb
 #define RESPONSE_BUFFER_SIZE (64 * 1024)
 
 // Request Parser Defines
 #define MAX_URI_LENGTH 2048
-
-#include <iostream>
-#include <csignal>
-#include <string>
-#include <unordered_map>
-#include <fstream>
-#include <sstream>
-#include <iostream>
-#include "Log.hpp"
-#include "colors.hpp"
 
 typedef std::unordered_map<std::string, std::string> HeaderMap;
 
