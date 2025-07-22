d5f97fd76c61b98f4e317b7f829acf3bf7a9fa9b
upperdim <45048352+upperdim@users.noreply.github.com>
2025-07-22 17:48:49 +0200

    followed valve style development

--- tmp/.coconut.cpp
+++ tmp/.coconut.cpp
@@ -1,2 +1,4 @@
 // This is a placeholder file in order to keep the tmp folder in the repo
 // This directory will be used to store temporary files for request bodies
+
+// Do not remove this file, see: load bearing coconut
