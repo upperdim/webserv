2f67b5985ba3f2df9e07e3da01575c8cac7df39a
upperdim <45048352+upperdim@users.noreply.github.com>
2025-07-26 21:00:36 +0200

    remove unused comments

--- src/network/ClientConnection.cpp
+++ src/network/ClientConnection.cpp
@@ -32,14 +32,12 @@ void ClientConnection::receiveRequest()
 		connectionError = true;
 		LOGT(Log::ERROR, "ClientConnection error, fd = " << fd);
 	} else if (bytesRead == 0) {
-		// LOGT(Log::INFO, "ClientConnection fd = " << fd << " has sent 0 bytes for " << zeroBytesReadCounter + 1 << " time(s).");
 		if (zeroBytesReadCounter < MAX_NUM_OF_TRIES_ON_ZERO_BYTES_READ) {
 			++zeroBytesReadCounter; // Try again on the next poll turn
-			// LOGT(Log::INFO, "It will try sending again on the next poll loop.");
 		} else {
 			disconnected = true;
 			LOGT(Log::INFO, "ClientConnection fd = " << fd << " has sent 0 bytes for " << zeroBytesReadCounter << " time(s).");
-			LOGT(Log::INFO, "ClientConnection fd = " << fd << " has disconnected");
+			LOGT(Log::INFO, "ClientConnection fd = " << fd << " has disconnected.");
 		}
 	}
 }
