c666102244af4969faeb3d0aa1d42a8326d91ae2
upperdim <45048352+upperdim@users.noreply.github.com>
2025-07-27 16:38:36 +0200

    update function call name

--- src/http/RequestParser.cpp
+++ src/http/RequestParser.cpp
@@ -103,7 +103,7 @@ void	RequestParser::parseHeader(Request& request)
 		return;
 
 	if (request.hasBody()) {
-		if (!request.createTempBodyFile() || !request.openBodyFile()) {
+		if (!request.createBodyFile() || !request.openBodyFile()) {
 			request.invalidateWithError(WSSC_INTERNAL_SERVER_ERROR);
 			return;
 		}
