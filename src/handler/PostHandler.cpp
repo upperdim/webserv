6394c74f65adbe6319bdc68c2c830f62cc41cc9d
nmihaile <nmihaile@student.42heilbronn.de>
2025-07-29 13:55:04 +0200

    reduces nesting and returns early if we dont have files to upload refines response creation if we dont have files to upload for autoindex and chacks for permissions refines some spellings

--- src/handler/PostHandler.cpp
+++ src/handler/PostHandler.cpp
@@ -25,65 +25,7 @@ void	PostHandler::handle(Request& request, Response& response)
 
 	std::string indexedResource = getIndexAppendedResource(request);
 
-	if (request.tmpUploadedFiles.size() > 0) {
-		// we should have a list of uploaded files
-
-		// logging the files we have to move
-		LOGT(Log::INFO, "moving the following " << LIGHTYELLOW << request.tmpUploadedFiles.size() << BLUE << " files to " << LIGHTYELLOW << request.resolvedPath);
-		for (auto f : request.tmpUploadedFiles)
-			LOGT(Log::INFO, "=> " << f);
-
-		std::vector<std::string> successfullyMoved;
-		successfullyMoved.reserve(request.tmpUploadedFiles.size());
-
-		size_t movedFileCount = 0;
-		for (const std::string& tmpPath : request.tmpUploadedFiles) {
-			std::filesystem::path source = tmpPath;
-			std::string filenameStr = source.filename().string();
-			if (filenameStr.length() > 21)
-				filenameStr.erase(filenameStr.length() - 21);
-			std::filesystem::path dest = std::filesystem::path(request.resolvedPath) / filenameStr;
-
-			try {
-				std::filesystem::rename(source, dest);
-				successfullyMoved.emplace_back(tmpPath);
-				LOGT(Log::SUCCESS, "Moved file to: " << dest);
-				++movedFileCount;
-			} catch(const std::exception& e) {
-				LOGT(Log::ERROR, "Failed to move file \"" << filenameStr << "\" to dest: " << dest << ": " << e.what());
-				createErrorResponse(request, response, WSSC_INTERNAL_SERVER_ERROR);
-				return;
-			}
-		}
-
-		LOGT(Log::INFO, "Successfully moved " << LIGHTGREEN << request.tmpUploadedFiles.size() << BLUE << " files.");
-
-		// erase succesfully moved files from Request class tmpUploadedFiles list
-		// we don't need to delete those files if error occures somewhere
-		for (const std::string& path : successfullyMoved) {
-			std::vector<std::string>::iterator it = std::find(
-				request.tmpUploadedFiles.begin(), request.tmpUploadedFiles.end(), path
-			);
-			if (it != request.tmpUploadedFiles.end()) {
-				request.tmpUploadedFiles.erase(it);
-			}
-		}
-
-		// return 206 Partial Content if we didn't succed moving all the files
-		if (request.tmpUploadedFiles.size() > 0) {
-			response.setStatusCode(WSSC_PARTIAL_CONTENT);
-			response.addHeader("Content-Type", HTTP::getMimeType(".html"));
-			response.setBodyString(HTTP::getPartialContentTemplate(successfullyMoved, request.tmpUploadedFiles));
-			return;
-		}
-
-		if (request.resolvedLocationBlock->autoIndex) {
-			// handle autoIndex
-			handleAutoIndex(request, response);
-			response.setStatusCode(WSSC_CREATED);
-			return;
-		}
-
+	if (request.tmpUploadedFiles.empty()) {
 		if (Utils::fileExists(indexedResource)) {
 			// check permission to serve the index
 			if (!Utils::hasPermission(indexedResource, R_OK)) {
@@ -91,23 +33,94 @@ void	PostHandler::handle(Request& request, Response& response)
 				return;
 			}
 
-			// create success response
-			response.setStatusCode(WSSC_CREATED);
+			response.setStatusCode(WSSC_NO_CONTENT);
 			response.addHeader("Content-Type", HTTP::getMimeType(indexedResource));
 			response.setBodyFileBufferReader(indexedResource);
 			return;
 		}
 
+		if (request.resolvedLocationBlock->autoIndex) {
+			// handle autoIndex
+			handleAutoIndex(request, response);
+			return;
+		}
+
+		// we dont have indexedResource and no autoIndex -> Forbidden
 		createErrorResponse(request, response, WSSC_FORBIDDEN);
 		return;
 	}
 
-	response.setStatusCode(WSSC_NO_CONTENT);
-	response.addHeader("Content-Type", HTTP::getMimeType(indexedResource));
-	response.setBodyFileBufferReader(indexedResource);
-	return;
+	// logging the files we have to move
+	LOGT(Log::INFO, "moving the following " << LIGHTYELLOW << request.tmpUploadedFiles.size() << BLUE << " files to " << LIGHTYELLOW << request.resolvedPath);
+	for (const auto& path : request.tmpUploadedFiles)
+		LOGT(Log::INFO, "=> " << path);
 
-	//	TODO:	we need to clean up tmpFiles if we abort or fail
+	std::vector<std::string> successfullyMoved;
+	successfullyMoved.reserve(request.tmpUploadedFiles.size());
 
-	//	TODO:	we need to clean the Request cass whene we support KEEP-ALIVE
+	size_t movedFileCount = 0;
+	for (const std::string& tmpPath : request.tmpUploadedFiles) {
+		std::filesystem::path source = tmpPath;
+		std::string filenameStr = source.filename().string();
+		if (filenameStr.length() > 21)
+			filenameStr.erase(filenameStr.length() - 21);
+		std::filesystem::path dest = std::filesystem::path(request.resolvedPath) / filenameStr;
+
+		try {
+			std::filesystem::rename(source, dest);
+			successfullyMoved.emplace_back(tmpPath);
+			LOGT(Log::SUCCESS, "Moved file to: " << dest);
+			++movedFileCount;
+		} catch(const std::exception& e) {
+			LOGT(Log::ERROR, "Failed to move file \"" << filenameStr << "\" to dest: " << dest << ": " << e.what());
+			createErrorResponse(request, response, WSSC_INTERNAL_SERVER_ERROR);
+			return;
+		}
+	}
+
+	LOGT(Log::INFO, "Successfully moved " << LIGHTGREEN << request.tmpUploadedFiles.size() << BLUE << " files.");
+
+	// erase successfully moved files from Request class tmpUploadedFiles list
+	// we don't need to delete those files if error occures somewhere
+	for (const std::string& path : successfullyMoved) {
+		std::vector<std::string>::iterator it = std::find(
+			request.tmpUploadedFiles.begin(), request.tmpUploadedFiles.end(), path
+		);
+		if (it != request.tmpUploadedFiles.end()) {
+			request.tmpUploadedFiles.erase(it);
+		}
+	}
+
+	// return 206 Partial Content if we didn't succed moving all the files
+	if (request.tmpUploadedFiles.size() > 0) {
+		response.setStatusCode(WSSC_PARTIAL_CONTENT);
+		response.addHeader("Content-Type", HTTP::getMimeType(".html"));
+		response.setBodyString(HTTP::getPartialContentTemplate(successfullyMoved, request.tmpUploadedFiles));
+		return;
+	}
+
+	if (request.resolvedLocationBlock->autoIndex) {
+		// handle autoIndex
+		handleAutoIndex(request, response);
+		response.setStatusCode(WSSC_CREATED);
+		return;
+	}
+
+	if (Utils::fileExists(indexedResource)) {
+		// check permission to serve the index
+		if (!Utils::hasPermission(indexedResource, R_OK)) {
+			createErrorResponse(request, response, WSSC_FORBIDDEN);
+			return;
+		}
+
+		// create success response
+		response.setStatusCode(WSSC_CREATED);
+		response.addHeader("Content-Type", HTTP::getMimeType(indexedResource));
+		response.setBodyFileBufferReader(indexedResource);
+		return;
+	}
+
+	createErrorResponse(request, response, WSSC_FORBIDDEN);
+
+	//	TODO:	we need to clean the Request class when we support KEEP-ALIVE
 }
