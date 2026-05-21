f9feea45e17c427243d5a27c79405992e6bd01c9
upperdim <45048352+upperdim@users.noreply.github.com>
2026-05-21 22:34:55 +0200

    improve comments

--- src/handler/GetHandler.cpp
+++ src/handler/GetHandler.cpp
@@ -1,5 +1,3 @@
-#include <filesystem>
-#include <algorithm>
 #include <unistd.h>	  // R_OK
 #include "GetHandler.hpp"
 #include "Utils.hpp"
@@ -20,13 +18,13 @@ void	GetHandler::handle(const Request& request, Response& response)
 		// 1. do we have index files we could serve?
 		std::string indexedResource = getIndexAppendedResource(request);
 		if (Utils::fileExists(indexedResource)) {
-			// check permission to serve the index
+			// Check permission to serve the index
 			if (!Utils::hasPermission(indexedResource, R_OK)) {
 				createErrorResponse(request, response, WSSC_FORBIDDEN);
 				return;
 			}
 
-			// we have permission and we serv the index
+			// We have permission and we serv the index
 			response.setStatusCode(WSSC_OK);
 			response.addHeader("Content-Type", HTTP::getMimeType(indexedResource));
 			response.setBodyFileBufferReader(indexedResource);
@@ -35,12 +33,11 @@ void	GetHandler::handle(const Request& request, Response& response)
 
 		// 2. is autoIndex enabled
 		if (request.resolvedLocationBlock->autoIndex) {
-			// handle autoIndex
 			handleAutoIndex(request, response);
 			return;
 		}
 
-		// we dont have indexedResource and no autoIndex -> Forbidden
+		// We dont have indexedResource and directory is not autoIndex -> Forbidden
 		createErrorResponse(request, response, WSSC_FORBIDDEN);
 		return;
 	}
@@ -50,7 +47,7 @@ void	GetHandler::handle(const Request& request, Response& response)
 	std::string resourceDir = request.resolvedPath.substr(0, lastSlashPos);
 	LOGT(Log::INFO, "resourceDir: " << resourceDir);
 
-	// does the directory of the file exist and do we have permissions for the directory
+	// Does the directory of the file exist and do we have permissions for the directory
 	if (!Utils::isDirectory(resourceDir)) {
 		createErrorResponse(request, response, WSSC_NOT_FOUND);
 		return;
@@ -61,7 +58,7 @@ void	GetHandler::handle(const Request& request, Response& response)
 		return;
 	}
 
-	// does the resource exist and do we have permissons
+	// Does the resource exist and do we have permissons
 	if (!Utils::fileExists(request.resolvedPath)) {
 		createErrorResponse(request, response, WSSC_NOT_FOUND);
 		return;
@@ -71,7 +68,8 @@ void	GetHandler::handle(const Request& request, Response& response)
 		createErrorResponse(request, response, WSSC_FORBIDDEN);
 		return;
 	}
-	// fetch content
+
+	// Fetch content
 	response.setStatusCode(WSSC_OK);
 	response.addHeader("Content-Type", HTTP::getMimeType(request.resolvedPath));
 	response.setBodyFileBufferReader(request.resolvedPath);
